64689d5660a553b21bb3b4c62af68fe648aa85d8
Phoethar <sayargyiphoelone@gmail.com>
2024-03-08 22:09:18 +0630

    Added- multi drag and drop for files upload

--- resources/js/Components/app/FileUploadMenuItem.vue
+++ resources/js/Components/app/FileUploadMenuItem.vue
@@ -1,13 +1,21 @@
 <template>
-    <MenuItem v-slot="{ active }" class="text-gray-700 block px-4 py-2 text-sm"
+    <MenuItem v-slot="{ active }" class="text-gray-700 block px-4 py-2 text-sm relative"
                     >
-        <a href="">Upload Files</a>
+        <a href="javascript:void(0);" class="text-gray-700 block px-4 py-2 text-sm">
+            Upload Files
+            <input @change="onChange" 
+                type="file" 
+                class="absolute left-0 top-0 bottom-0 right-0 cursor-pointer opacity-0"
+                multiple
+            >
+        </a>
     </MenuItem>
 </template>
   
 <script setup>
-// Imports
+// Importsbottom-0 right-0 cursor-pointer
 import { MenuItem } from '@headlessui/vue';
+import { FILE_UPLOAD_STARTED, emitter } from '@/event-bus.js';
 
 // Uses
 
@@ -18,6 +26,9 @@ import { MenuItem } from '@headlessui/vue';
 // Computed
 
 // Methods
+function onChange(event) {
+    emitter.emit(FILE_UPLOAD_STARTED, event.target.files);
+}
 
 // Hooks
 
--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -1,20 +1,84 @@
 <template>
     <div class="h-screen bg-gray-50 flex w-full gap-4">
         <Navigation />
-        <main class="flex flex-col flex-1 px-4 overflow-hidden">
-            <div class="flex items-center justify-between w-full">
-                <SearchForm />
-                <UserSettingsDropDown />
-            </div>
-            <div class="flex-1 flex flex-col overflow-hidden">
-                <slot />
-            </div>
+        <main @drop.prevent="handleDrop"
+              @dragover.prevent="onDragover"
+              @dragleave.prevent="onDragLeave"
+            class="flex flex-col flex-1 px-4 overflow-hidden"
+            :class="dragOver ? 'dropzone' : ''"
+        >
+            <template v-if="dragOver" class="text-gray-500 text-center py-8 text-sm">
+                Drop Files Here To Upload!
+            </template>
+            <template v-else>
+                <div class="flex items-center justify-between w-full">
+                    <SearchForm />
+                    <UserSettingsDropDown />
+                </div>
+                <div class="flex-1 flex flex-col overflow-hidden">
+                    <slot />
+                </div>
+            </template>
         </main>
     </div>
 </template>
+
 <script setup>
+// Imports
 import Navigation from '@/Components/app/Navigation.vue';
 import SearchForm from '@/Components/app/SearchForm.vue';
 import UserSettingsDropDown from '@/Components/app/UserSettingsDropDown.vue';
+import { FILE_UPLOAD_STARTED, emitter } from '@/event-bus';
+import {handleError, onMounted, ref} from "vue";
+
+// Uses
+
+// Refs
+const dragOver = ref(false);
+
+// Props & Emit
+
+// Computed
+
+// Methods
+function handleDrop(event) {
+    dragOver.value = false;
+    const files = event.dataTransfer.files;
+    console.log(files);
+
+    if (!files.length) {
+        return;
+    }
+    uploadFiles(files);
+}
+
+function onDragover() {
+    dragOver.value = true;
+}
+
+function onDragLeave() {
+    dragOver.value = false;
+}
+
+function uploadFiles(files) {
+    console.log(files);
+}
+
+// Hooks
+onMounted( () => {
+    emitter.on(FILE_UPLOAD_STARTED, uploadFiles);
+});
 
 </script>
+
+<style scoped>
+    .dropzone {
+        width: 100%;
+        height: 100%;
+        color: #8d8d8d;
+        border: 2px dotted gray;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+</style>
